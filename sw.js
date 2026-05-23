bad9dadb2532e6c4ad566f6529d6efa2d803d3d4
smartmindhq-cmd <smartmindhq@gmail.com>
2026-05-23 18:41:05 -0400

    Bump SW cache to v32

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "forecast-v31";
+const CACHE = "forecast-v32";
 const ASSETS = ["/", "/index.html", "/icon-180.png", "/icon-192.png", "/icon-512.png", "/manifest.json"];
 
 self.addEventListener("install", e => {
